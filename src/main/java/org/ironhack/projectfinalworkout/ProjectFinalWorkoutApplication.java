70f2813e7283ed2d6fc7f0e6afb794d43fb78440
luisalejosm <luisaalejosm41@gmail.com>
2024-11-02 08:54:47 +0100

    Primer Commit

---
+++ src/main/java/org/ironhack/projectfinalworkout/ProjectFinalWorkoutApplication.java
@@ -0,0 +1,13 @@
+package org.ironhack.projectfinalworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinalWorkoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFinalWorkoutApplication.class, args);
+    }
+
+}
