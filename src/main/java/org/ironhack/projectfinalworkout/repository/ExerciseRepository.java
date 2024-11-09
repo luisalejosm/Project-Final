f69a50fb61adea020835c32c8962da5d632f9e36
luisalejosm <luisaalejosm41@gmail.com>
2024-11-09 11:15:25 +0100

    correccion repositorio exercise

--- src/main/java/org/ironhack/projectfinalworkout/repository/ExerciseRepository.java
+++ src/main/java/org/ironhack/projectfinalworkout/repository/ExerciseRepository.java
@@ -3,5 +3,9 @@ package org.ironhack.projectfinalworkout.repository;
 import org.ironhack.projectfinalworkout.model.Exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+
+    List<Exercise> findAll();
 }
